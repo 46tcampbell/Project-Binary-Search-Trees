78b0a8fe998b1a53f8fcf5baab671478f5a6998c
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2026-02-04 11:22:49 -0600

    Update the insert method of the tree class
    I originally thought I would be cute and use the existing includes
    method to do an initial search of the tree to return immediately if
    the value already existed in the tree. While this part is efficient, the
    way I had the code would mean that the includes method would run each
    time the recursive call hits which was not what I intended. To solve,
    I just removed the method call and placed a new line of code in there.
    
    In the future, just make sure my desire to be DRY works out best for the
    overall solution.

--- tree.js
+++ tree.js
@@ -38,18 +38,19 @@ export class Tree {
       return this.root;
     }
     // Below is to just return immediately if the value is already in the tree
-    if (this.includes(value)) return;
+    if (value === root.data)
+      return console.log(`Value: ${root.data}. It's already in here, Bruh`);
     if (value > root.data) {
       if (root.right === null) {
         root.right = new Node(value);
-        return;
+        return console.log(`New node added with value: ${root.right.data}`);
       } else {
         return this.insert(value, root.right);
       }
     } else {
       if (root.left === null) {
         root.left = new Node(value);
-        return;
+        return console.log(`New node added with value: ${root.left.data}`);
       } else {
         return this.insert(value, root.left);
       }
